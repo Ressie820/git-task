f0b87f0a604a2559ed1a07ff57ea254de97092de
Ressie Mashigo <ressiemashigo@ymail.com>
2026-08-07 20:22:52 +0200

    Fix print statement in hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,1 @@
-# Ask for the user's name
-name = input("please enter your name:")
-
-# Print the user's name
-print("your name is:", name)
-
-# Ask for the user's age
-age = input("please enter your age:")
-
-# Print the user's age
-print("your age is:", age)
-
-# Print Hello, world!
-print("Hello, World!")
+print"Hello world" 
